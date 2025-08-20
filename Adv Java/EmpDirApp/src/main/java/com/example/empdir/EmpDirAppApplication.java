190f86b90da7def38f61d89789ccea141c3380ea
Ali-shah-sa <sayyedalisha0000@gmail.com>
2025-08-20 18:34:49 +0530

    Add files via upload

---
+++ Adv Java/EmpDirApp/src/main/java/com/example/empdir/EmpDirAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.empdir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDirAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDirAppApplication.class, args);
+	}
+
+}
