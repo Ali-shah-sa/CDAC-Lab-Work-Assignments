410840d7e7f28b0ed4f0e1d0eb66015b58639b7b
Ali-shah-sa <sayyedalisha0000@gmail.com>
2025-08-20 18:43:16 +0530

    Delete FirstApp directory

--- FirstApp/src/Components/FunctionBased.js
+++
@@ -1,8 +0,0 @@
-function FunctionBased(props){
-    return(
-        <div className="App-header1">
-            <h3>This is Function based Component Stateless and name From Props is {props.name}</h3>
-        </div>
-    );
-}
-export default FunctionBased;
